e693967b5b222a2a2996c10fb6312f141f6fbbab
Andreas <andreas.wombacher@aureliusenterprise.com>
2022-07-19 22:24:11 +0200

    updated sample config and credentials

--- scripts/config.sample.py
+++ scripts/config.sample.py
@@ -1,22 +1,23 @@
 config = {
-    "atlas.server.url": "YOUR_SERVER_URL",
-    "kafka.bootstrap.server.hostname": "YOUR_BOOTSTRAP_SERVER_HOSTNAME",
-    "kafka.bootstrap.server.port": 1234,
-    "kafka.consumer.group.id": "YOUR_GROUP_ID",
-    "atlas.audit.events.topic.name": "YOUR_TOPIC_NAME",
-    "enriched.events.topic.name": "YOUR_TOPIC_NAME",
-    "determined.events.topic.name": "YOUR_TOPIC_NAME",
-    "exception.events.topic.name": "YOUR_TOPIC_NAME",
-    
-    "elastic_search_index" : "YOUR_ELASTIC_SEARCH_INDEX",
+    "atlas.credentials.username": "admin", # only relevant if used for a local docker instance
+    "atlas.server.url": "127.0.0.1:21000/api/atlas",
+    "kafka.bootstrap.server.hostname": "127.0.0.1",
+    "kafka.bootstrap.server.port": "9027",
+    "kafka.consumer.group.id": None,
+    "atlas.audit.events.topic.name": "ATLAS_ENTITIES",
+    "enriched.events.topic.name": "ENRICHED_ENTITIES",
+    "determined.events.topic.name": "DETERMINED_CHANGE",
+    "exception.events.topic.name": "DEAD_LETTER_BOX",
 
-    "elastic_cloud_username" : "YOUR_ELASTIC_USER_NAME",
-    "elastic_cloud_id" : "YOUR_ELASTIC_CLOUD_ID",
-    "elastic_base_endpoint" : "YOUR_ELASTIC_BASE_ENDPOINT",
+    "elastic_search_index" : "atlas-dev-test",
 
-    "keycloak.server.url" : "KEYCLOAK_SERVER_URL",
-    "keycloak.client.id" : "KEYCLOAK_CLIENT_ID",
-    "keycloak.realm.name": "KEYCLOAK_REALM_NAME"
+    "elastic_cloud_username": "elastic",
+    "elastic_cloud_id": "YOUR CLOUD ID",
+    "elastic_base_endpoint" : "APP-SEACRH-HOSTNAME/api/as/v1",
+
+    "keycloak.server.url" : "http://127.0.0.1:9100/auth/",
+    "keycloak.client.id" : "m4i_public",
+    "keycloak.realm.name": "m4i",
 }
 
 
