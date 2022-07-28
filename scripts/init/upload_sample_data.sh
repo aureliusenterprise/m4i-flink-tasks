bca635b0aa00d19de644b65a2b209c66c0585166
Andreas <andreas.wombacher@aureliusenterprise.com>
2022-07-28 18:47:08 +0200

    updated script

--- scripts/init/upload_sample_data.sh
+++ scripts/init/upload_sample_data.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-TOKEN = curl -d 'client_id=m4i_public' -d 'username=atlas' -d 'password=Ywc0aPJ3xw1LNFfS' -d 'grant_type=password' \
+TOKEN=curl -d 'client_id=m4i_public' -d 'username=atlas' -d 'password=Ywc0aPJ3xw1LNFfS' -d 'grant_type=password' \
                 'https://aureliusdev.westeurope.cloudapp.azure.com/anwo/auth/realms/m4i/protocol/openid-connect/token'
 
-curl -g -X POST -H 'Authorization: Bearer $TOKEN' \
+curl -g -v -X POST -H "Authorization: Bearer ${TOKEN:1:-1}" \
                 -H "Content-Type: multipart/form-data" \
                 -H "Cache-Control: no-cache" \
                 -F data=@data/response1.zip \
