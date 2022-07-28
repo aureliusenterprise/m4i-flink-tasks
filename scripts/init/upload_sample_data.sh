4d4e9e7af0899a2261419d14abadd6c8631c449a
Andreas <andreas.wombacher@aureliusenterprise.com>
2022-07-28 19:50:38 +0200

    update

--- scripts/init/upload_sample_data.sh
+++ scripts/init/upload_sample_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-TOKEN=curl -d 'client_id=m4i_public' -d 'username=atlas' -d 'password=Ywc0aPJ3xw1LNFfS' -d 'grant_type=password' \
-                'https://aureliusdev.westeurope.cloudapp.azure.com/anwo/auth/realms/m4i/protocol/openid-connect/token'
+TOKEN=curl -d 'client_id=m4i_public' -d "username=$KEYCLOAK_ATLAS_USER_USERNAME" -d "password=$KEYCLOAK_ATLAS_USER_PASSWORD" -d 'grant_type=password' \
+                'https://aureliusdev.westeurope.cloudapp.azure.com/anwo/auth/realms/m4i/protocol/openid-connect/token' | jq .access_token
 
 curl -g -v -X POST -H "Authorization: Bearer ${TOKEN:1:-1}" \
                 -H "Content-Type: multipart/form-data" \
