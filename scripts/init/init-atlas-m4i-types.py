b4b925ba8a01e1252ddc1aaa0b4807451c666fa2
gabriellaPaschaVanoord <87004337+gabriellaPaschaVanoord@users.noreply.github.com>
2022-07-28 16:09:20 +0200

    token keycloak

--- scripts/init/init-atlas-m4i-types.py
+++ scripts/init/init-atlas-m4i-types.py
@@ -9,10 +9,20 @@ store = ConfigStore.get_instance()
 store.load({
     "atlas.credentials.username": os.getenv('KEYCLOAK_ATLAS_USER_USERNAME'),
     "atlas.credentials.password": os.getenv('KEYCLOAK_ATLAS_ADMIN_PASSWORD'),
-    "atlas.server.url": os.getenv('ATLAS_EXTERNAL_URL') + "/api/atlas"
+    "atlas.server.url": os.getenv('ATLAS_EXTERNAL_URL') + "/api/atlas",
+    "keycloak.server.url": os.getenv('KEYCLOAK_SERVER_URL'),
+    "keycloak.client.id": 'm4i_public',
+    "keycloak.realm.name": 'm4i',
+    "keycloak.client.secret.key": None
 })
 
-asyncio.run(create_type_defs(data_dictionary_types_def))
-asyncio.run(create_type_defs(process_types_def))
-asyncio.run(create_type_defs(connectors_types_def))
-asyncio.run(create_type_defs(kubernetes_types_def))
+atlas_user, atlas_password = store.get_many(
+    "atlas.credentials.username",
+    "atlas.credentials.password",
+    all_required=True
+)
+access_token = get_keycloak_token(keycloak=None, credentials=(atlas_user, atlas_password))
+asyncio.run(create_type_defs(data_dictionary_types_def, access_token=access_token))
+asyncio.run(create_type_defs(process_types_def, access_token=access_token))
+asyncio.run(create_type_defs(connectors_types_def, access_token=access_token))
+asyncio.run(create_type_defs(kubernetes_types_def, access_token=access_token))
