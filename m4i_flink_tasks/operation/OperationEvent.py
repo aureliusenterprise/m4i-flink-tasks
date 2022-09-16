f7f5656aeabc0f9ef5b681453a3f10bcfea1bcd3
Andreas <andreas.wombacher@aureliusenterprise.com>
2022-09-16 13:57:49 +0200

    changed operations to operation such that sequence can be placed there.

--- m4i_flink_tasks/operation/OperationEvent.py
+++ m4i_flink_tasks/operation/OperationEvent.py
@@ -12,7 +12,7 @@ class OperationChange(DataClassJsonMixin):
     propagate: bool
     propagate_down: bool
     
-    operations: List[dict]
+    operation: dict
 
 
 @dataclass_json(letter_case=LetterCase.CAMEL)
@@ -25,13 +25,13 @@ class OperationEvent(DataClassJsonMixin):
     changes: List[OperationChange]
     
 
-oc = OperationChange(propagate=True, propagate_down=True, operations = [{"hello": "workld"}])
-ocj = oc.to_json()
+# oc = OperationChange(propagate=True, propagate_down=True, operation = {"hello": "workld"})
+# ocj = oc.to_json()
 
-oe = OperationEvent(id=str(uuid.uuid4()), 
-                    creation_time=int(datetime.now().timestamp()*1000),
-                    entity_guid="d56db187-2627-41a6-8698-f74d4b76227e",
-                    changes=[oc])
-oej = oe.to_json()
+# oe = OperationEvent(id=str(uuid.uuid4()), 
+#                     creation_time=int(datetime.now().timestamp()*1000),
+#                     entity_guid="d56db187-2627-41a6-8698-f74d4b76227e",
+#                     changes=[oc])
+# oej = oe.to_json()
 
-oe2 = OperationEvent.from_json(oej)
+# oe2 = OperationEvent.from_json(oej)
