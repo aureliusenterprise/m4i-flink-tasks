b410dc202726ba9776a329af2ad831482b8f7c30
Andreas <andreas.wombacher@aureliusenterprise.com>
2022-09-20 22:09:33 +0200

    isolation of getEntityLocal from get entity job to enable testing

--- scripts/get_entity_job.py
+++ scripts/get_entity_job.py
@@ -1,92 +1,92 @@
-import asyncio
-import json
 import logging
 import sys
 import os
+import time
+import traceback
 
-from pyflink.common.typeinfo import Types
+from kafka import KafkaProducer
 
 from config import config
 from credentials import credentials
-from m4i_atlas_core import AtlasChangeMessage, ConfigStore, EntityAuditAction, get_entity_by_guid, get_keycloak_token
+from m4i_atlas_core import ConfigStore
+from m4i_flink_tasks.operation.GetEntityLocal import GetEntityLocal
+
+from m4i_flink_tasks.DeadLetterBoxMessage import DeadLetterBoxMesage
 store = ConfigStore.get_instance()
 
-import time
-import traceback
-from kafka import KafkaProducer
 from pyflink.common.serialization import SimpleStringSchema
 from pyflink.datastream import StreamExecutionEnvironment
 from pyflink.datastream.connectors import FlinkKafkaConsumer, FlinkKafkaProducer
 from pyflink.datastream.functions import MapFunction, RuntimeContext
-from m4i_flink_tasks.DeadLetterBoxMessage import DeadLetterBoxMesage
+from pyflink.common.typeinfo import Types
 
 
 
-class WrongOperationTypeException(Exception):
-    pass
+# class WrongOperationTypeException(Exception):
+#     pass
 
-class GetEntityLocal(object):
-    access_token = None
+# class GetEntityLocal(object):
+#     access_token = None
 
-    def get_access_token(self):
-        if self.access_token==None:
-            try:
-                self.access_token = get_keycloak_token()
-            except:
-                pass
-        return self.access_token
+#     def get_access_token(self):
+#         if self.access_token==None:
+#             try:
+#                 self.access_token = get_keycloak_token()
+#             except:
+#                 pass
+#         return self.access_token
 
-    def map_local(self, kafka_notification: str):
+#     def map_local(self, kafka_notification: str):
 
-        async def get_entity(kafka_notification, access_token):
+#         async def get_entity(kafka_notification, access_token):
 
-            logging.info(repr(kafka_notification))
-            kafka_notification_obj = AtlasChangeMessage.from_json(kafka_notification)
-            logging.info(access_token)
+#             logging.info(repr(kafka_notification))
+#             kafka_notification_obj = AtlasChangeMessage.from_json(kafka_notification)
+#             logging.info(access_token)
 
-            if kafka_notification_obj.message.operation_type in [EntityAuditAction.ENTITY_CREATE, EntityAuditAction.ENTITY_UPDATE]:
-                msg_creation_time = kafka_notification_obj.msg_creation_time
-                entity_guid = kafka_notification_obj.message.entity.guid
-                await get_entity_by_guid.cache.clear()
-                event_entity = await get_entity_by_guid(guid=entity_guid, ignore_relationships=False, access_token=access_token)
-                # event_entity = await get_entity_by_guid(guid=entity_guid, ignore_relationships=False)
-                if not event_entity:
-                    raise Exception(f"No entity could be retreived from Atlas with guid {entity_guid}")
+#             if kafka_notification_obj.message.operation_type in [EntityAuditAction.ENTITY_CREATE, EntityAuditAction.ENTITY_UPDATE]:
+#                 msg_creation_time = kafka_notification_obj.msg_creation_time
+#                 entity_guid = kafka_notification_obj.message.entity.guid
+#                 await get_entity_by_guid.cache.clear()
+#                 event_entity = await get_entity_by_guid(guid=entity_guid, ignore_relationships=False, access_token=access_token)
+#                 # event_entity = await get_entity_by_guid(guid=entity_guid, ignore_relationships=False)
+#                 if not event_entity:
+#                     raise Exception(f"No entity could be retreived from Atlas with guid {entity_guid}")
 
-                logging.warning(repr(kafka_notification_obj))
-                logging.warning(repr(event_entity))
-                kafka_notification_json = json.loads(kafka_notification_obj.to_json())
-                entity_json = json.loads(event_entity.to_json())
+#                 logging.warning(repr(kafka_notification_obj))
+#                 logging.warning(repr(event_entity))
+#                 kafka_notification_json = json.loads(kafka_notification_obj.to_json())
+#                 entity_json = json.loads(event_entity.to_json())
 
-                logging.warning(json.dumps({"kafka_notification" : kafka_notification_json, "atlas_entity" : entity_json}))
-                return json.dumps({"kafka_notification" : kafka_notification_json, "atlas_entity" : entity_json, "msg_creation_time": msg_creation_time})
+#                 logging.warning(json.dumps({"kafka_notification" : kafka_notification_json, "atlas_entity" : entity_json}))
+#                 return json.dumps({"kafka_notification" : kafka_notification_json, "atlas_entity" : entity_json, "msg_creation_time": msg_creation_time})
 
-            elif kafka_notification_obj.message.operation_type == EntityAuditAction.ENTITY_DELETE:
-                kafka_notification_json = json.loads(kafka_notification_obj.to_json())
-                logging.warning(json.dumps({"kafka_notification" : kafka_notification_json, "atlas_entity" : {}}))
-                return json.dumps({"kafka_notification" : kafka_notification_json, "atlas_entity" : {}, "msg_creation_time": msg_creation_time})
+#             elif kafka_notification_obj.message.operation_type == EntityAuditAction.ENTITY_DELETE:
+#                 kafka_notification_json = json.loads(kafka_notification_obj.to_json())
+#                 logging.warning(json.dumps({"kafka_notification" : kafka_notification_json, "atlas_entity" : {}}))
+#                 return json.dumps({"kafka_notification" : kafka_notification_json, "atlas_entity" : {}, "msg_creation_time": msg_creation_time})
 
-            else:
-                logging.warning("message with an unexpected message operation type")
-                raise WrongOperationTypeException(f"message with an unexpected message operation type  received from Atlas with guid {kafka_notification_obj.message.entity.guid} and operation type {kafka_notification.message.operation_type}")
-        # END func
+#             else:
+#                 logging.warning("message with an unexpected message operation type")
+#                 raise WrongOperationTypeException(f"message with an unexpected message operation type  received from Atlas with guid {kafka_notification_obj.message.entity.guid} and operation type {kafka_notification.message.operation_type}")
+#         # END func
 
-        logging.info(repr(store))
+#         logging.info(repr(store))
 
-        retry = 0
-        while retry < 3:
-            try:
-                access__token = self.get_accress_token()
-                return asyncio.run(get_entity(kafka_notification, access__token))
-            except WrongOperationTypeException as e:
-                raise e
-            except Exception as e:
-                logging.error("failed to retrieve entity from atlas - retry")
-                logging.error(str(e))
-                self.access_token = None
-            retry = retry+1
-        raise Exception(f"Failed to lookup entity for kafka notification {kafka_notification}")
-# end of class GetEntityLocal
+#         retry = 0
+#         while retry < 3:
+#             try:
+#                 access__token = self.get_accress_token()
+#                 return asyncio.run(get_entity(kafka_notification, access__token))
+#             except WrongOperationTypeException as e:
+#                 raise e
+#             except Exception as e:
+#                 logging.error("failed to retrieve entity from atlas - retry")
+#                 logging.error(str(e))
+#                 self.access_token = None
+#             retry = retry+1
+#         raise Exception(f"Failed to lookup entity for kafka notification {kafka_notification}")
+# # end of class GetEntityLocal
 
 
 class GetEntity(MapFunction,GetEntityLocal):
---
+++ scripts/test__get_entity_job.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from m4i_flink_tasks.operation.GetEntityLocal import GetEntityLocal
+
+from config import config
+from credentials import credentials
+from m4i_atlas_core import AtlasChangeMessage, ConfigStore, EntityAuditAction, get_entity_by_guid, get_keycloak_token
+from m4i_flink_tasks.DeadLetterBoxMessage import DeadLetterBoxMesage
+# store = ConfigStore.get_instance()
+# store.load({**config, **credentials})
+
+@pytest.fixture(autouse=True)
+def store():
+    store = ConfigStore.get_instance()
+    store.load({**config, **credentials})
+
+    yield store
+
+    store.reset()
+    
+    
+def test__get_entity():
+    msg = '{"version":{"version":"1.0.0","versionParts":[1]},"msgCompressionKind":"NONE","msgSplitIdx":1,"msgSplitCount":1,"msgSourceIP":"10.244.2.233","msgCreatedBy":"","msgCreationTime":1663702503306,"spooled":false,"message":{"type":"ENTITY_NOTIFICATION_V2","entity":{"typeName":"hdfs_path","attributes":{"path":"gfhj","createTime":1663624800000,"qualifiedName":"fghj","name":"fghj"},"guid":"8732e707-7a0c-417e-882a-6ac4017c8d15","displayText":"fghj","isIncomplete":false},"operationType":"ENTITY_CREATE","eventTime":1663702503065}}'
+    cl = GetEntityLocal()
+    res= cl.map_local(msg)
+    cl.get_access_token()
