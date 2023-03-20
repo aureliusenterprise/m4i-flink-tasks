fd3f7612e35203911bce8307151581cf3050a859
Thijs <thijs.franck@aureliusenterprise.com>
2023-03-20 15:31:45 +0100

    wip

--- scripts/determine_change_job.py
+++ scripts/determine_change_job.py
@@ -1,11 +1,10 @@
 import logging
-import os
 import sys
 import time
 import traceback
 
 from kafka import KafkaProducer
-from m4i_atlas_core import ConfigStore
+from m4i_atlas_core import ConfigStore, retry_decorator
 from pyflink.common.serialization import Encoder, SimpleStringSchema
 from pyflink.common.typeinfo import Types
 from pyflink.datastream import StreamExecutionEnvironment
@@ -20,15 +19,6 @@ from m4i_flink_tasks import DeadLetterBoxMesage
 from m4i_flink_tasks.operation.DetermineChangeLocal import DetermineChangeLocal
 
 store = ConfigStore.get_instance()
-
-
-def retry(func: Retryable, retries: int = 1, *args, **kwargs):
-    for _ in range(retries):
-        try:
-            func(*args, **kwargs)
-            break
-        except Exception as e:
-            logging.error(f"error in {func.__name__}: {repr(e)}")
 
 
 class DetermineChange(MapFunction, DetermineChangeLocal):
@@ -55,6 +45,7 @@ class DetermineChange(MapFunction, DetermineChangeLocal):
         )
     # END __init__
 
+    @retry_decorator(retries=2)
     def send_deadletter_message(self, event: DeadLetterBoxMesage):
         self.kafka_producer.send(
             topic=self.dead_lettter_box_topic,
@@ -73,133 +64,181 @@ class DetermineChange(MapFunction, DetermineChangeLocal):
             job="determine_change",
             description=formatted_exception,
             exception_class=type(e).__name__,
-            remark=None
+            remark=""
         )
 
         logging.error(f"this goes into dead letter box: {repr(event)}")
 
-        for _ in range(2):
-            try:
-                self.send_deadletter_message(event)
-                break
-            except Exception as e2:
-                logging.error(
-                    f"error dumping data into deadletter topic: {repr(e2)}")
+        try:
+            self.send_deadletter_message(event)
+        except Exception as e:
+            logging.error(
+                f"error dumping data into deadletter topic: {repr(e)}"
+            )
+        # END TRY
+    # END handle_error
 
     def map(self, kafka_notification: str):
-        self.cnt = self.cnt + 1
-        logging.info(f"recevied events DetermineChange: {self.cnt}")
         try:
             res = self.map_local(kafka_notification)
-            logging.info("received result: "+repr(res))
             return res
         except Exception as e:
-            logging.error("Exception during processing:")
-            logging.error(repr(e))
-
-            exc_info = sys.exc_info()
-            e1 = (''.join(traceback.format_exception(*exc_info)))
-
-            event = DeadLetterBoxMesage(timestamp=time.time(), original_notification=kafka_notification, job="determine_change", description=(e1),
-                                        exception_class=type(e).__name__, remark=None)
-            logging.error("this goes into dead letter box: ")
-            logging.error(repr(event))
-
-            retry = 0
-            while retry < 2:
-                try:
-                    producer_ = self.get_deadletter()
-                    producer_.send(
-                        topic=self.dead_lettter_box_topic, value=event.to_json())
-                    return
-                except Exception as e2:
-                    logging.error(
-                        "error dumping data into deadletter topic "+repr(e2))
-                    retry = retry + 1
+            self.handle_error(e, kafka_notification)
+        # END TRY
+    # END map
 # end of class DetermineChange
 
 
 class GetResultDetermineChange(FlatMapFunction):
-    cnt = 0
-
     def flat_map(self, input_list):
         for element in input_list:
-            self.cnt = self.cnt+1
-            logging.info(
-                f"submitted event count GetResultDetermineChange: {self.cnt}")
             yield element
+        # END LOO
+    # END flat_map
+# END GetResultDetermineChange
 
 
-def run_determine_change_job(output_path):
-    env = StreamExecutionEnvironment.get_execution_environment()
-    env.set_parallelism(1)
+def create_kafka_source():
+    bootstrap_server_hostname, bootstrap_server_port, kafka_consumer_group_id, source_topic_name = store.get_many(
+        "kafka.bootstrap.server.hostname",
+        "kafka.bootstrap.server.port",
+        "kafka.consumer.group.id",
+        "enriched.events.topic.name",
+        all_required=True
+    )
 
-    path = os.path.dirname(__file__)
+    consumer_properties = {
+        "bootstrap.servers": f"{bootstrap_server_hostname}:{bootstrap_server_port}",
+        "group.id": kafka_consumer_group_id
+    }
 
-    # download JARs
-    kafka_jar = "file:///" + path + "/../flink_jars/flink-connector-kafka-1.15.1.jar"
-    kafka_client = "file:///" + path + "/../flink_jars/kafka-clients-2.2.1.jar"
+    consumer = FlinkKafkaConsumer(
+        topics=source_topic_name,
+        properties=consumer_properties
+    )
 
-    env.add_jars(kafka_jar, kafka_client)
+    (
+        consumer
+        .set_commit_offsets_on_checkpoints(True)
+        .set_start_from_latest()
+    )
 
-    bootstrap_server_hostname = config.get("kafka.bootstrap.server.hostname")
-    bootstrap_server_port = config.get("kafka.bootstrap.server.port")
-    source_topic_name = config.get("enriched.events.topic.name")+"_SAVED"
-    sink_topic_name = config.get("determined.events.topic.name")
-    kafka_consumer_group_id = config.get("kafka.consumer.group.id")
+    return consumer
+# END create_kafka_source
 
-    kafka_source = FlinkKafkaConsumer(topics=source_topic_name,
-                                      properties={'bootstrap.servers':  f"{bootstrap_server_hostname}:{bootstrap_server_port}",
-                                                  'group.id': kafka_consumer_group_id+"_determine_change_job",
-                                                  "key.deserializer": "org.apache.kafka.common.serialization.StringDeserializer",
-                                                  "value.deserializer": "org.apache.kafka.common.serialization.StringDeserializer"},
-                                      deserialization_schema=SimpleStringSchema())
-    if kafka_source == None:
-        logging.warning("kafka source is empty")
-        logging.warning(
-            f"bootstrap_servers: {bootstrap_server_hostname}:{bootstrap_server_port}")
-        logging.warning(f"group.id: {kafka_consumer_group_id}")
-        logging.warning(f"topcis: {source_topic_name}")
-        raise Exception("kafka source is empty")
-    kafka_source.set_commit_offsets_on_checkpoints(
-        True).set_start_from_latest()
 
-    data_stream = env.add_source(kafka_source).name(
-        "consuming enriched atlas events determine_change")
+def create_kafka_sink():
+    bootstrap_server_hostname, bootstrap_server_port, sink_topic_name = store.get_many(
+        "kafka.bootstrap.server.hostname",
+        "kafka.bootstrap.server.port",
+        "determined.events.topic.name",
+        all_required=True
+    )
 
-    data_stream = data_stream.map(DetermineChange(), Types.LIST(
-        element_type_info=Types.STRING())).name("determine change").filter(lambda notif: notif)
+    producer_properties = {
+        "bootstrap.servers": f"{bootstrap_server_hostname}:{bootstrap_server_port}",
+        "max.request.size": "14999999",
+    }
 
-    data_stream = data_stream.flat_map(GetResultDetermineChange(
-    ), Types.STRING()).name("parse change determine_change")
+    return FlinkKafkaProducer(
+        topic=sink_topic_name,
+        producer_config=producer_properties,
+        serialization_schema=SimpleStringSchema(),
+    )
+# END create_kafka_sink
 
-    data_stream.add_sink(FlinkKafkaProducer(topic=sink_topic_name,
-                                            producer_config={"bootstrap.servers": f"{bootstrap_server_hostname}:{bootstrap_server_port}",
-                                                             "max.request.size": "14999999", 'group.id': kafka_consumer_group_id+"_determine_change_job2"},
-                                            serialization_schema=SimpleStringSchema())).name("write_to_kafka_sink determine_change")
 
-    # define the sink
-    if output_path is not None:
-        data_stream.add_sink(
-            sink_func=FileSink.for_row_format(
-                base_path=output_path,
-                encoder=Encoder.simple_string_encoder())
-            .with_output_file_config(
-                OutputFileConfig.builder()
-                .with_part_prefix("determine_change_result_")
-                .with_part_suffix(".ext")
-                .build()
-            )
-            .with_rolling_policy(RollingPolicy.default_rolling_policy())
-            .build()
+def create_file_sink():
+    sink = FileSink.for_row_format(
+        base_path=output_path,
+        encoder=Encoder.simple_string_encoder()
+    )
+    .with_output_file_config(
+        OutputFileConfig.builder()
+        .with_part_prefix("determine_change_result_")
+        .with_part_suffix(".ext")
+        .build()
+    )
+    .with_rolling_policy(RollingPolicy.default_rolling_policy())
+    .build()
+
+    return sink
+# END create_file_sink
+
+
+class DetermineChangePipeline:
+
+    def __init__(self, env: StreamExecutionEnvironment, changes_source: FlinkKafkaConsumer):
+        self.changes = (
+            env
+            .add_source(changes_source)
+            .name("consuming enriched atlas events determine_change")
+            .map(DetermineChange(), Types.LIST(element_type_info=Types.STRING()))
         )
 
-    env.execute("determine_change")
+        self.parsed_changes = (
+            self.changes
+            .filter(lambda notif: notif)
+            .flat_map(GetResultDetermineChange(), Types.STRING())
+            .name("parse change determine_change")
+        )
+    # END __init__
+
+# END DetermineChangeJob
+
+
+def create_stream_execution_environment():
+    env = StreamExecutionEnvironment.get_execution_environment()
+
+    # PLACEHOLDER CONFIG KEY
+    parallelism = store.get("determine.changes.parallelism", default=1)
+    env.set_parallelism(parallelism)
+
+    # PLACEHOLDER CONFIG KEY
+    dependencies = store.get("determine.changes.dependencies", default=[])
+    env.add_jars(*dependencies)
+
+    return env
+# END create_stream_execution_environment
+
+
+def create_determine_change_job(output_path: str):
+
+    env = create_stream_execution_environment()
+    kafka_source = create_kafka_source()
+
+    stream = DetermineChangePipeline(env, kafka_source)
+
+    # If output_path is None, sink to Kafka. Else, sink to file.
+    if output_path is None:
+
+        kafka_sink = create_kafka_sink()
+
+        (
+            stream.parsed_changes
+            .add_sink(kafka_sink)
+            .name("write_to_kafka_sink determine_change")
+        )
+    else:
+        file_sink = create_file_sink()
+
+        (
+            stream.parsed_changes
+            .add_sink(sink_func=file_sink)
+            .name("write_to_file_sink determine_change")
+        )
+    # END IF
+
+    return env
+# END exec
 
 
 if __name__ == '__main__':
-    logging.basicConfig(stream=sys.stdout,
-                        level=logging.INFO, format="%(message)s")
+    logging.basicConfig(
+        stream=sys.stdout,
+        level=logging.INFO,
+        format="%(message)s"
+    )
     logging.debug(sys.argv)
 
     debug = False
@@ -213,5 +252,7 @@ if __name__ == '__main__':
     if debug:
         output_path = '/tmp/'
 
-    run_determine_change_job(output_path)
+    job = create_determine_change_job(output_path)
+
+    job.execute("determine_change")
 # END MAIN
