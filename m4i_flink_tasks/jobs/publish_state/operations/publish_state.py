533867b70db187d87e04ad3ed04c703fd23426d7
Thijs Franck <37906715+thijs-franck@users.noreply.github.com>
2023-03-26 10:46:06 +0000

    added documentation for the publish state class

--- m4i_flink_tasks/jobs/publish_state/operations/publish_state.py
+++ m4i_flink_tasks/jobs/publish_state/operations/publish_state.py
@@ -8,12 +8,39 @@ from ....model import AtlasChangeMessageWithPreviousVersion
 
 
 class PublishState(MapFunction):
+    """
+    A class that implements a PyFlink MapFunction for retrieving the previous version of an entity from Elasticsearch for further processing.
+
+    Attributes:
+        elastic (Elasticsearch): An instance of the Elasticsearch client.
+        index_name (str): The name of the Elasticsearch index to query.
+    """
+
     def __init__(self, elastic: Elasticsearch, index_name: str):
+        """
+        Initialize the PublishState class.
+
+        Args:
+            elastic (Elasticsearch): An instance of the Elasticsearch client.
+            index_name (str): The name of the Elasticsearch index to query.
+        """
+
         self.elastic = elastic
         self.index_name = index_name
     # END __init__()
 
     def map(self, value: str) -> str:
+        """
+        Process an AtlasChangeMessageWithPreviousVersion and update its 'previous_version' attribute
+        with the previous version of the entity retrieved from Elasticsearch.
+
+        Args:
+            value (str): A JSON-formatted string that represents the AtlasChangeMessageWithPreviousVersion.
+
+        Returns:
+            str: A JSON-formatted string that represents the updated AtlasChangeMessageWithPreviousVersion.
+        """
+
         change_message = AtlasChangeMessageWithPreviousVersion.from_json(value)
         entity = change_message.message.entity
 
@@ -28,6 +55,16 @@ class PublishState(MapFunction):
     # END map
 
     def get_previous_atlas_entity(self, guid: str, timestamp: int) -> Optional[Entity]:
+        """
+        Retrieve the previous version of an entity from Elasticsearch using its GUID and a timestamp.
+
+        Args:
+            guid (str): The GUID of the entity to search for.
+            timestamp (int): The timestamp to use as a threshold for finding previous versions.
+
+        Returns:
+            Optional[Entity]: The previous version of the entity if found, or None if no previous versions are available.
+        """
 
         query = {
             "bool": {
